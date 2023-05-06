0ddd67184e00922cb6c301b657caa2428a552876
Pranav Pore <pranav@pranavpore.com>
2023-05-06 22:28:50 +0530

    updated code

--- middleware/index.js
+++ middleware/index.js
@@ -17,31 +17,31 @@ const isOuth = (req,res,next)=>{
     
     const user = users.find(u=>u.api_key==key);
     if(user) next();
-    else res.send("Not Authorized");
+    else res.send("Not authorized!");
 }
 
 const validateSignUp = (req,res,next)=>{
     const {name,email,password} = req.body;
-    if(!name || !email || !password) res.send("Invalid Feilds!!");
+    if(!name || !email || !password) res.send("Invalid fields!");
     else {
         const users = JSON.parse(
             fs.readFileSync(path.join(__dirname,"../Users.json"),{encoding : "UTF-8"})
         );
-        if(users.find(user=>user.email==email)) return res.send("User Already exists!!");
+        if(users.find(user=>user.email==email)) return res.send("User already exists!");
         return next();
     }
 };
 
 const validateLogIn = (req,res,next)=>{
     const {email,password} = req.body;
-    if(!email || !password) res.send("Invalid LogIn!!");
+    if(!email || !password) res.send("Invalid ogin credentials!");
     else return next();
 };
 
 const isAuthorized = (req,res,next)=>{
     const {auth:token} = req.headers;
     if(verifyJwt(token)) return next();
-    else return res.send("Access denied!!");
+    else return res.send("Access denied!");
 }
 module.exports = {
     some,
